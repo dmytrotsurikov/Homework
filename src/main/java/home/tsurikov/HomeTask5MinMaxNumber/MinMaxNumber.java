ba44db6a237450f7bfd772d3f7fb18ef2863d27c
dmytrotsurikov <dtsurikov@hotmail.com>
2019-10-03 02:24:04 +0300

    Long

--- src/main/java/home/tsurikov/HomeTask5MinMaxNumber/MinMaxNumber.java
+++ src/main/java/home/tsurikov/HomeTask5MinMaxNumber/MinMaxNumber.java
@@ -12,22 +12,24 @@ import java.util.Scanner;
 public class MinMaxNumber {
     public static void main(String[] args) {
 
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number");
-        Long number=scanner.nextLong();
+        Long number = scanner.nextLong();
 
-       int max, min;
-       int num= (int)(number%10);
+        int max = 0;
+        int min = 0;
+        int num = (int) (number % 10);
 
-        max = min = num;
-        while(num>0){
-            if(max < num) max = num;
-            if(min > num) min = num;
-            num=num/10;
+        while (num > 0) {
+            if (min > num) {
+                min = num;
+            }
+            if (max < num) {
+                max = num;
+            }
+            num /= 10;
         }
-
-        System.out.println("max = "+ max + " min =" + min);
-
+        System.out.println("Max: " + max + "Min: " + min);
 
 
     }
