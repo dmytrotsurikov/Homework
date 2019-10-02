d2dad54d344f968751ac264e65ccefa734f303d0
dmytrotsurikov <dtsurikov@hotmail.com>
2019-10-03 02:04:05 +0300

    Long

--- src/main/java/home/tsurikov/HomeTask5MinMaxNumber/MinMaxNumber.java
+++ src/main/java/home/tsurikov/HomeTask5MinMaxNumber/MinMaxNumber.java
@@ -7,8 +7,28 @@
 
 package home.tsurikov.HomeTask5MinMaxNumber;
 
+import java.util.Scanner;
+
 public class MinMaxNumber {
     public static void main(String[] args) {
+
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter number");
+        Long number=scanner.nextLong();
+
+       int max, min;
+       int num= (int)(number%10);
+
+        max = min = num;
+        while(num>0){
+            if(max < num) max = num;
+            if(min > num) min = num;
+            num=num/10;
+        }
+
+        System.out.println("max = "+ max + " min =" + min);
+
+
 
     }
 }
