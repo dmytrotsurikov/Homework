09ce16c2d36686d89de9b6432dc5fa34b21cba63
dmytrotsurikov <dtsurikov@hotmail.com>
2019-10-04 19:31:32 +0300

    Final version with  ternary operator

--- src/main/java/home/tsurikov/polindrom/Polindrom.java
+++ src/main/java/home/tsurikov/polindrom/Polindrom.java
@@ -1,36 +1,42 @@
+/* Пользователь вводит последовательность символов.
+        Программа должна определить, является ли последовательность палиндромом.
+        Вывести ДА, если последовательность символов палиндром и НЕТ в противном случае.
+*/
+
 package home.tsurikov.polindrom;
 // арозаупаланалапуазора
 import java.util.Scanner;
 
 public class Polindrom {
 
+    private static final String POSITIVE_ANSWER="Your word is palindrom";
+    private static final String NEGATIVE_ANSWER="No,Your word isn't palindrom";
+
     public static void main(String[] args) {
 
-        boolean da = isPalindrome(getString ()) ;
-        System.out.println(da);
+        String resultMessage = isPalindrome(getString ()) ? POSITIVE_ANSWER : NEGATIVE_ANSWER;
+        System.out.println(resultMessage);
 
         }
 
     public static String getString () {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter word");
+        System.out.println("Enter your word");
         String string = scanner.nextLine();
         return string;
-
     }
 
      public static boolean isPalindrome(String string) {
         int n = string.length();
 
         for (int i = 0; i < (n/2); ++i) {
-            char ch= string.charAt(i);
-            char chlast =string.charAt(n-i-1);
+            char ch = string.charAt(i);
+            char chlast = string.charAt(n-i-1);
             if (ch != chlast) {
                 return false;
             }
         }
-
         return true;
     }
 }
