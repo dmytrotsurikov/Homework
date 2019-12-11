e50d5d1931ed959f6911b738502acd3a4d3e67c3
dmytrotsurikov <dtsurikov@hotmail.com>
2019-12-11 03:16:50 +0200

    Grep

---
+++ src/main/java/home/tsurikov/grep/MyGrep.java
@@ -0,0 +1,49 @@
+/*
+Задача 7.1
+Разработать программу, аналог grep :).
+Программа ожидает 2 параметра на входе:
+-имя файла (относительное либо полный путь)
+-строку/регулярное выражение
+
+После программа выводить все строки (с номером строки в файле), в которых найдена строка либо регулярное выражение.
+Если файл не найден, либо доступ к нему запрещен, программа выводит соответствующее сообщение.
+
+https://docs.oracle.com/javase/7/docs/api/java/io/LineNumberReader.html
+https://javadevblog.com/kak-postrochno-schity-vat-fajl-v-java.html
+ */
+package home.tsurikov.grep;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class MyGrep {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Введите путь к файлу"); // C:\\Users\\Dmitriy\\Desktop\\test.txt
+            Scanner scanner = new Scanner(System.in);
+            String path = scanner.nextLine();
+            // построчное считывание файла
+            //создаем LineNumberReader с существующего FileReader для построчного считывания
+           LineNumberReader lineNumberReader = new LineNumberReader(new FileReader(path));
+
+            System.out.println("Введите выражение");
+            String expression = scanner.nextLine();
+
+            String line;
+            int lineNumber;
+
+            while ((line = lineNumberReader.readLine()) != null) {
+                if (line.contains(expression)) {
+                    lineNumber = lineNumberReader.getLineNumber();
+                    System.out.println(lineNumber + " " + line);
+                 }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
